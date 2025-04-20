391a1d762a8dc5246060dfb7c67f2c1a623f34cb
Oksana <volkova.oksana98@yandex.ru>
2025-04-20 23:53:59 +0300

    Первый коммит 14 спринта

--- src/main/java/ru/practicum/shareit/exceptions/EmailExistsException.java
+++ src/main/java/ru/practicum/shareit/exceptions/EmailExistsException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
-public class EmailExistsException extends RuntimeException{
+public class EmailExistsException extends RuntimeException {
     public EmailExistsException(String message) {
         super(message);
     }
